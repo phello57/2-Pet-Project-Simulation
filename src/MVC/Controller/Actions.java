db82d50c529e71667d3a96ee6eb7d6f36d3c0ad8
phello57 <lobanchikov.artem@gmail.com>
2024-06-15 15:35:44 +0500

    .

--- src/MVC/Controller/Actions.java
+++ src/MVC/Controller/Actions.java
@@ -12,7 +12,7 @@ public class Actions {
         createEntities(pMap, Settings.SPAWN_BEAR, Settings.SPAWN_ROCK, Settings.SPAWN_PIG, Settings.SPAWN_TREE, Settings.SPAWN_GRASS);
     }
     public static void addEntities(GameMap pMap) {
-        //createEntities(pMap, 0, 0, 5, 0, 20);
+        createEntities(pMap, 0, 0, 5, 0, 20);
     }
     private static void createEntities(GameMap pMap, int pBear, int pRock, int pPig, int pTree, int pGrass) {
 
